325a4297a40a38420c4cc945b3ae23463072a9e2
jjdavenport <jordanjamesdavenport@gmail.com>
2024-07-21 21:49:18 +0100

    mobile layout added

--- src/App.jsx
+++ src/App.jsx
@@ -7,81 +7,84 @@ import iconVisual from "./assets/images/icon-visual.svg";
 function App() {
   return (
     <>
-      <main>
-        <div className="left">
-          <h2>Your Result</h2>
-          <div className="result-div">
-            <h1>
-              76
-              <span>of 100</span>
-            </h1>
+      <div className="flex flex-col min-h-screen">
+        <main className="flex-grow flex flex-col">
+          <div className="result bg-gradient-light-slate-blue text-white font-hanken flex flex-col items-center flex-1 gap-4 rounded-b-3xl p-6">
+            <h2 className="opacity-80">Your Result</h2>
+            <div className="result-div">
+              <h1 className="flex flex-col text-center text-8xl">
+                76
+                <span className="text-xl opacity-80">of 100</span>
+              </h1>
+            </div>
+            <h3>Great</h3>
+            <p className="text-center">
+              You scored higher than 65% of the people who have taken these
+              tests.
+            </p>
           </div>
-          <h3>Great</h3>
-          <p>
-            You scored higher than 65% of the people who have taken these tests.
-          </p>
-        </div>
-        <div className="right">
-          <div className="h1-div">
-            <h2>Summary</h2>
+          <div className="summary flex-1 text-center p-4 flex flex-col gap-4">
+            <div className="h1-div">
+              <h2 className="text-start">Summary</h2>
+            </div>
+            <div className="flex justify-between" id="react">
+              <div className="flex gap-2">
+                <img src={iconReaction} alt="" />
+                <p>Reaction</p>
+              </div>
+              <div className="right-col">
+                <p>
+                  80
+                  <span> / 100</span>
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-between" id="memory">
+              <div className="flex gap-2">
+                <img src={iconMemory} alt="" />
+                <p>Memory</p>
+              </div>
+              <div className="right-col">
+                <p>
+                  92
+                  <span> / 100</span>
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-between" id="verbal">
+              <div className="flex gap-2">
+                <img src={iconVerbal} alt="" />
+                <p>Verbal</p>
+              </div>
+              <div className="right-col">
+                <p>
+                  61
+                  <span> / 100</span>
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-between" id="visual">
+              <div className="flex gap-2">
+                <img src={iconVisual} alt="" />
+                <p>Visual</p>
+              </div>
+              <div className="right-col">
+                <p>
+                  72
+                  <span> / 100</span>
+                </p>
+              </div>
+            </div>
+            <button>Continue</button>
           </div>
-          <div className="catagories" id="react">
-            <div className="left-col">
-              <img src={iconReaction} alt="" />
-              <p>Reaction</p>
-            </div>
-            <div className="right-col">
-              <p>
-                80
-                <span>/ 100</span>
-              </p>
-            </div>
-          </div>
-          <div className="catagories" id="memory">
-            <div className="left-col">
-              <img src={iconMemory} alt="" />
-              <p>Memory</p>
-            </div>
-            <div className="right-col">
-              <p>
-                92
-                <span>/ 100</span>
-              </p>
-            </div>
-          </div>
-          <div className="catagories" id="verbal">
-            <div className="left-col">
-              <img src={iconVerbal} alt="" />
-              <p>Verbal</p>
-            </div>
-            <div className="right-col">
-              <p>
-                61
-                <span>/ 100</span>
-              </p>
-            </div>
-          </div>
-          <div className="catagories" id="visual">
-            <div className="left-col">
-              <img src={iconVisual} alt="" />
-              <p>Visual</p>
-            </div>
-            <div className="right-col">
-              <p>
-                72
-                <span>/ 100</span>
-              </p>
-            </div>
-          </div>
-          <a>Continue</a>
-        </div>
-      </main>
-      <div className="attribution">
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="https://github.com/jjdavenport">jjdavenport</a>.
+        </main>
+        <footer className="text-center text-xs h-min">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="https://github.com/jjdavenport">jjdavenport</a>.
+        </footer>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this line to match your project structure
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust paths to your project's structure
+    "./public/index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightRed: "hsl(0, 100%, 67%)",
+        orangeyYellow: "hsl(39, 100%, 56%)",
+        greenTeal: "hsl(166, 100%, 37%)",
+        cobaltBlue: "hsl(234, 85%, 45%)",
+        white: "hsl(0, 0%, 100%)",
+        paleBlue: "hsl(221, 100%, 96%)",
+        lightLavender: "hsl(241, 100%, 89%)",
+        darkGrayBlue: "hsl(224, 30%, 27%)",
+      },
+      backgroundImage: {
+        "gradient-light-slate-blue":
+          "linear-gradient(to bottom, hsl(252, 100%, 67%), hsl(241, 81%, 54%))",
+        "gradient-violet-blue":
+          "linear-gradient(to bottom, hsla(256, 72%, 46%, 1), hsla(241, 72%, 46%, 0))",
+      },
+      fontFamily: {
+        hanken: ["Hanken Grotesk", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
